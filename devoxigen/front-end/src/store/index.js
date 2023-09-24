e30f571d03efa9c5cba97c1586f4837e68f3b0df
Qweexexe <105299343+Qweexexe@users.noreply.github.com>
2023-09-24 17:12:14 +0000

    Edited file

--- devoxigen/front-end/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers, createStore} from "redux";
-import {headReducer} from "./reducers/headReducer";
-
-const RootReducer = combineReducers({
-    head : headReducer
-})
-
-export const store = createStore(RootReducer)
